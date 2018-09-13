452136a09429a7a6f0cbebdb8650320d54460cf5
producer <producer@highsierra1.content.linkedin.biz>
2018-09-13 18:46:41 +0200

    DE_2985_03_06_Querylist_changes

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,31 +1,48 @@
-import { Component, EventEmitter, OnInit, Output, ViewEncapsulation } from '@angular/core';
-import {User} from '../user';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { User } from '../user';
+import { UserNameComponent } from '../user-name/user-name.component';
 
 @Component ( {
-  selector: 'in-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: [ './user-list.component.scss' ],
+  selector     : 'in-user-list',
+  templateUrl  : './user-list.component.html',
+  styleUrls    : [ './user-list.component.scss' ],
   encapsulation: ViewEncapsulation.Emulated
-})
-export class UserListComponent implements OnInit {
+} )
+export class UserListComponent implements OnInit, AfterViewInit {
 
   @Output ()
   selectUsr: EventEmitter<User> = new EventEmitter ();
 
+  @ViewChildren ( UserNameComponent )
+  userNames: QueryList<UserNameComponent>;
+
   users: User[] = [
-    { name: 'Frank Müller', age: 12},
-    { name: 'Hans Müller', age: 13},
-    { name: 'Peter Müller', age: 14},
-    { name: 'Paul Müller', age: 15}
+    { name: 'Frank Müller', age: 12 },
+    { name: 'Hans Müller', age: 13 },
+    { name: 'Peter Müller', age: 14 },
+    { name: 'Paul Müller', age: 15 }
   ];
 
   selectedUsr: User;
 
-  constructor() { }
-  ngOnInit() {  }
+  constructor () {
+  }
 
-  onSelectedUsr ( selectedUsr: User) {
-    this.selectedUsr = selectedUsr;
-    this.selectUsr.emit( this.selectedUsr );
+  ngOnInit () {
+  }
+
+  onSelectedUsr ( selectedUsr: User ) {
+    // this.selectedUsr = selectedUsr;
+    // this.selectUsr.emit ( this.selectedUsr );
+    // this.users.pop();
+    this.users.push( {age: 44, name: 'Saban Ünlü'});
+  }
+
+  ngAfterViewInit (): void {
+    this.userNames.changes.subscribe(
+      next => {
+        console.log ( next );
+      }
+    );
   }
 }
--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,42 +1,15 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { UserListComponent } from '../user-list/user-list.component';
-import { Subscription } from 'rxjs/internal/Subscription';
+import { Component, OnInit } from '@angular/core';
 
 @Component ( {
   selector   : 'in-user',
   templateUrl: './user.component.html',
   styleUrls  : [ './user.component.scss' ]
 } )
-export class UserComponent implements OnInit, AfterViewInit, OnDestroy {
+export class UserComponent implements OnInit {
 
-  @ViewChild ( UserListComponent )
-  userList: UserListComponent;
-
-  @ViewChild ( UserListComponent, { read: ElementRef } )
-  userListRef: ElementRef;
-
-  @ViewChild ( 'helloWorld' )
-  paragraphElementRef: ElementRef<HTMLParagraphElement;
-
-  private selectionSub: Subscription;
-
-  constructor ( private renderer: Renderer2 ) {
+  constructor ( ) {
   }
 
   ngOnInit () {
-  }
-
-  ngAfterViewInit (): void {
-    this.selectionSub = this.userList.selectUsr.subscribe (
-      next => console.log ( 'selected', next )
-    );
-
-    console.log ( this.paragraphElementRef );
-
-    this.renderer.setStyle ( this.paragraphElementRef.nativeElement, 'color', 'red' );
-  }
-
-  ngOnDestroy (): void {
-    this.selectionSub.unsubscribe ();
   }
 }
