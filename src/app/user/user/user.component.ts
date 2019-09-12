12fac1f0fb19b6ca4b1636a08dd521031381c224
producer <producer@highsierra1.content.linkedin.biz>
2019-09-12 13:31:13 +0200

    update ng8

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { UserListComponent } from '../user-list/user-list.component';
 import { Subscription } from 'rxjs';
 
@@ -9,24 +9,33 @@ import { Subscription } from 'rxjs';
 } )
 export class UserComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  @ViewChild ( UserListComponent, { static: false })
+  @ViewChild ( UserListComponent, { static: false } )
   userList: UserListComponent;
+
+  @ViewChild ( UserListComponent, { static: true, read: ElementRef } )
+  userListRef: ElementRef;
+
   private selectionSub: Subscription;
 
-  constructor () {
+  constructor ( private elemRef: ElementRef, private renderer: Renderer2 ) {
   }
 
   ngOnInit () { // { static: true }
     // console.log ( this.userList );
     // console.log ( this.userList.selectUsr );
+    console.log ( this.userListRef );
+    this.renderer.setStyle( this.userListRef.nativeElement, 'color', 'red' );
   }
 
   ngAfterViewInit (): void { // { static: false }
-    this.selectionSub = this.userList.selectUsr.subscribe( next => console.log ('selected', next ) );
+    this.selectionSub = this.userList.selectUsr
+                            .subscribe ( next => console.log ( 'selected', next ) );
+    // console.log ( this.elemRef );
+    // this.renderer.setStyle( this.elemRef.nativeElement, 'color', 'red' );
   }
 
   ngOnDestroy (): void {
-    this.selectionSub.unsubscribe();
+    this.selectionSub.unsubscribe ();
   }
 
 }
