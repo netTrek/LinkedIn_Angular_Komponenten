6682b879fff88948b9810d18ce42ae1ae4cfe014
producer <producer@highsierra1.content.linkedin.biz>
2019-09-12 17:12:03 +0200

    Vorlage_ueber_Direktive ng 8

--- src/app/dynamic/dynamic/template-base/template-base.component.ts
+++ src/app/dynamic/dynamic/template-base/template-base.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, OnInit, TemplateRef } from '@angular/core';
+import { Component, ContentChild, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
 import { AddressDirective } from '../../address.directive';
 
 @Component ( {
@@ -10,13 +10,21 @@ export class TemplateBaseComponent implements OnInit {
 
   myContext = { $implicit: 'Deutschland', name: 'Saban Ünlü' };
 
-  @ContentChild ( AddressDirective, { static: false, read: TemplateRef } )
-  myTemp: TemplateRef<HTMLElement>;
+  @ContentChild ( AddressDirective, { static: true, read: TemplateRef } )
+  myTemp: TemplateRef<any>;
 
-  constructor () {
+  @ViewChild ( TemplateRef, { static: true, read: ViewContainerRef } )
+  private viewContainerRef: ViewContainerRef;
+
+  constructor ( /*private viewContainerRef: ViewContainerRef*/ ) {
   }
 
   ngOnInit () {
+    this.viewContainerRef.createEmbeddedView ( this.myTemp, this.myContext );
+    const viewRef: ViewRef = this.myTemp.createEmbeddedView ( {
+      $implicit: 'USA', name: 'Peter Müller'
+    } );
+    this.viewContainerRef.insert ( viewRef );
   }
 
 }
